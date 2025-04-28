22ae1de72878eeb652aa4b0cfd7c08b669680419
Cipher <dipanshusaini69@gmail.com>
2025-04-28 16:16:35 +0530

    creating a bucket using python

--- src.py
+++ src.py
@@ -3,13 +3,18 @@
 # install cli for aws 
 # aws configure 
 
-
 import boto3
 
-s3 = boto3.resource('s3')
+client = boto3.client('s3')
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+response = client.create_bucket(
+    Bucket='dipsbhaikipehlibucket',
+     CreateBucketConfiguration={
+        'LocationConstraint': 'ap-south-1',
+    },
+)
+
+print(response)
+
 
 print("End of script")
