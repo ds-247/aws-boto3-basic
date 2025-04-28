fda93b5ce18d5a9225cba43bf0e5c6dde6ba7dd2
Cipher <dipanshusaini69@gmail.com>
2025-04-28 15:53:23 +0530

    accessing the s3 service and listing all the buckets

--- src.py
+++ src.py
@@ -1,1 +1,15 @@
-print("hello aws")
+# create venv
+# pip install robo3
+# install cli for aws 
+# aws configure 
+
+
+import boto3
+
+s3 = boto3.resource('s3')
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+print("End of script")
