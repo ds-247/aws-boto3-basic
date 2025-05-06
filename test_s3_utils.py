aef49a57b4c827fd46fbc69a138d43a4bf293896
Cipher <dipanshusaini69@gmail.com>
2025-05-07 00:48:36 +0530

    * adjusted the test files according to refactoring of s3_utils

--- test_s3_utils.py
+++ test_s3_utils.py
@@ -10,36 +10,41 @@ from s3_utils import (
     delete_object,
 )
 
-bucket_name = 'test-bucket'
-region = 'ap-south-1'
-num_of_files = 5
+BUCKET_NAME = 'test-bucket'
+REGION = 'ap-south-1'
+NUM_OF_FILES = 5
+TAG_KEY = 'type'
+TAG_VALUE_ODD = 'odd'
+TAG_VALUE_EVEN = 'even'
 
 @mock_aws
 def test_create_bucket():
-    s3 = boto3.client('s3', region_name=region)
-    create_bucket(s3, bucket_name, region)
+    s3 = boto3.client('s3', region_name=REGION)
+    success = create_bucket(s3, BUCKET_NAME, REGION)
+    assert success is True
     response = s3.list_buckets()
     bucket_names = [bucket['Name'] for bucket in response['Buckets']]
-    
-    assert bucket_name in bucket_names
+    assert BUCKET_NAME in bucket_names
 
 
 @mock_aws
 def test_upload_files():
-    s3 = boto3.client('s3', region_name=region)
-    create_bucket(s3, bucket_name, region)
-    upload_files(s3, bucket_name, number_of_files=num_of_files)
-    response = s3.list_objects(Bucket=bucket_name)
-    assert len(response.get('Contents', [])) == num_of_files
+    s3 = boto3.client('s3', region_name=REGION)
+    create_bucket(s3, BUCKET_NAME, REGION)
+    success = upload_files(s3, BUCKET_NAME, number_of_files=NUM_OF_FILES)
+    assert success is True
+    response = s3.list_objects(Bucket=BUCKET_NAME)
+    assert len(response.get('Contents', [])) == NUM_OF_FILES
 
 
 @mock_aws
 def test_list_bucket_objects():
-    s3 = boto3.client('s3', region_name=region)
-    create_bucket(s3, bucket_name, region)
-    upload_files(s3, bucket_name, number_of_files=num_of_files)
-    objects = list_bucket_objects(s3, bucket_name)
-    assert len(objects) == num_of_files
+    s3 = boto3.client('s3', region_name=REGION)
+    create_bucket(s3, BUCKET_NAME, REGION)
+    upload_files(s3, BUCKET_NAME, number_of_files=NUM_OF_FILES)
+    objects = list_bucket_objects(s3, BUCKET_NAME)
+    assert objects is not None
+    assert len(objects) == NUM_OF_FILES
     for obj in objects:
         assert 'Key' in obj
         assert 'Tags' in obj
@@ -49,11 +54,11 @@ def test_list_bucket_objects():
 @mock_aws
 def test_filter_objects():
     objects = [
-        {'Key': 'file1.txt', 'Tags': {'type': 'odd'}},
-        {'Key': 'file2.txt', 'Tags': {'type': 'even'}},
-        {'Key': 'file3.txt', 'Tags': {'type': 'odd'}}
+        {'Key': 'file1.txt', 'Tags': {TAG_KEY: TAG_VALUE_ODD}},
+        {'Key': 'file2.txt', 'Tags': {TAG_KEY: TAG_VALUE_EVEN}},
+        {'Key': 'file3.txt', 'Tags': {TAG_KEY: TAG_VALUE_ODD}}
     ]
-    filtered = filter_objects(objects, 'type', 'odd')
+    filtered = filter_objects(objects, TAG_KEY, TAG_VALUE_ODD)
     assert len(filtered) == 2
     assert filtered[0]['Key'] == 'file1.txt'
     assert filtered[1]['Key'] == 'file3.txt'
@@ -61,13 +66,14 @@ def test_filter_objects():
 
 @mock_aws
 def test_delete_object():
-    s3 = boto3.client('s3', region_name=region)
-    create_bucket(s3, bucket_name, region)
-    upload_files(s3, bucket_name, number_of_files=num_of_files)
-    all_objects = list_bucket_objects(s3,bucket_name)
-    to_delete = filter_objects(all_objects, 'type', 'odd')
-    delete_object(s3, bucket_name, to_delete)
-    remaining = s3.list_objects(Bucket=bucket_name).get('Contents', [])
+    s3 = boto3.client('s3', region_name=REGION)
+    create_bucket(s3, BUCKET_NAME, REGION)
+    upload_files(s3, BUCKET_NAME, number_of_files=NUM_OF_FILES)
+    all_objects = list_bucket_objects(s3,BUCKET_NAME)
+    to_delete = filter_objects(all_objects, TAG_KEY, TAG_VALUE_ODD)
+    success =  delete_object(s3, BUCKET_NAME, to_delete)
+    assert success is True
+    remaining = s3.list_objects(Bucket=BUCKET_NAME).get('Contents', [])
     remaining_keys = [obj['Key'] for obj in remaining]
     deleted_keys = [obj['Key'] for obj in to_delete]
     for key in deleted_keys:
